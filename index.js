ca7d498acd42de08b5eef5e116f772a6c21c8f5b
Usukhuu64 <24lp0723@nest.edu.mn>
2025-03-17 23:52:22 +0800

    third commit

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-let reset = document.getElementById("reset-button");
-
-reset.addEventListener("click", () => {});
-
 let result = document.getElementsByClassName("result");
 
-let billInput = document.getElementById("bill-input");
+let percentaged5 = document.getElementById("five-percentage");
+let percentaged10 = document.getElementById("ten-percentage");
+let percentaged15 = document.getElementById("fifteen-percentage");
+let percentaged25 = document.getElementById("twentyfive-percentage");
+let percentaged35 = document.getElementById("thirtyfive-percentage");
+let percentaged50 = document.getElementById("fifty-percentage");
+let percentaged65 = document.getElementById("sixtyfive-percentage");
+let percentaged80 = document.getElementById("eighty-percentage");
 
-let percentaged5 = document.getElementsByClassName("five-percentage");
-let percentaged10 = document.getElementsByClassName("ten-percentage");
-let percentaged15 = document.getElementsByClassName("fifteen-percentage");
-let percentaged25 = document.getElementsByClassName("twentyfive-percentage");
-let percentaged35 = document.getElementsByClassName("thirtyfive-percentage");
-let percentaged50 = document.getElementsByClassName("fifty-percentage");
-let percentaged65 = document.getElementsByClassName("sixtyfive-percentage");
-let percentaged80 = document.getElementsByClassName("eighty-percentage");
+let billInput = Number(document.getElementById("bill-input").value);
+
+percentaged5.addEventListener("click", () => {
+  let i = billInput * 1.05; // Add 5%
+  document.getElementById("result").textContent = i; // Display result
+});
+
+// let reset = document.getElementById("reset-button");
+// reset.addEventListener("click", () => {});
