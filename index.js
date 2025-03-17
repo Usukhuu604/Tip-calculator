b48637d13b7d22214b0e9979d183f594dc70f8e9
24LP0723 24LP0723 <24lp0723nest.edu.mn>
2025-03-17 12:30:52 +0800

    second commit

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ let reset = document.getElementById("reset-button");
 reset.addEventListener("click", () => {});
 
 let result = document.getElementsByClassName("result");
+
+let billInput = document.getElementById("bill-input");
+
+let percentaged5 = document.getElementsByClassName("five-percentage");
+let percentaged10 = document.getElementsByClassName("ten-percentage");
+let percentaged15 = document.getElementsByClassName("fifteen-percentage");
+let percentaged25 = document.getElementsByClassName("twentyfive-percentage");
+let percentaged35 = document.getElementsByClassName("thirtyfive-percentage");
+let percentaged50 = document.getElementsByClassName("fifty-percentage");
+let percentaged65 = document.getElementsByClassName("sixtyfive-percentage");
+let percentaged80 = document.getElementsByClassName("eighty-percentage");
